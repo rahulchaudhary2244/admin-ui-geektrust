2e53d97203f269d0d0023151094a543011d6edfd
Rahul Chaudhary <rahulchaudhary2244@gmail.com>
2024-08-12 01:52:51 +0530

    initial changes for pagination without filtering

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
-import "@/app/globals.css";
+import "./globals.css";
 import { cn } from "@/lib/utils";
 
 export const metadata: Metadata = {
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import { AdminTable } from "@/components/admin-table/admin-table";
 import { Input } from "@/components/ui/input";
+import { Suspense } from "react";
 
-export default function Home() {
+export default async function Home() {
     return (
         <div className="mx-auto p-8">
             <Input placeholder="Search by name, email or role" />
+            <Suspense fallback={<div>Loading 2</div>}>
+                <AdminTable />
+            </Suspense>
         </div>
     );
 }
